0ae95d91b84bac62b2f8a39c13af9c142a18d26c
Vanilla <vanilla@nya.gg>
2025-07-03 15:54:59 +0200

    Fix logging setup

--- src/main/java/gg/nya/tgirlclicker/controller/PageController.java
+++ src/main/java/gg/nya/tgirlclicker/controller/PageController.java
@@ -1,13 +1,16 @@
 package gg.nya.tgirlclicker.controller;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    Logger log = org.slf4j.LoggerFactory.getLogger(PageController.class);
 
     @GetMapping("/")
     public String index() {
+        log.info("Test Log");
         return "index"; // This will resolve to src/main/resources/templates/index.html
     }
 }
